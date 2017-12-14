ce64be66d59ed77460be616cc367699f3dddb303
Jon Jagger <jon@jaggersoft.com>
2017-12-14 21:47:01 +0000

    added notes

--- server/src/starter.rb
+++ server/src/starter.rb
@@ -6,7 +6,7 @@ class Starter
   end
 
   def languages(major_name, minor_name)
-    #returns a major-list (languages) and a minot-list (testFrameworks)
+    #returns a major-list (languages) and a minor-list (testFrameworks)
     #and initial indexes for both lists.
   end
 
@@ -33,3 +33,47 @@ class Starter
   end
 
 end
+
+=begin
+Notes
+views/setup_default_start_point/show_languages.html.erb
+        <%= render partial: 'shared/start_points_major_list' %>
+views/shared/start_points_major_list.html.erb
+
+  <% @start_points.major_names.each_with_index do |major_name, index| %>
+    <div class="filename"
+         id="major_<%= index %>"
+         data-major="<%= major_name %>"
+         data-index="<%= index %>"
+         data-minor-index="<%= @start_points.minor_indexes[index][0] %>">
+      <%= major_name %>
+    </div>
+  <% end %>
+
+From controller
+    languages_names = display_names_of(languages)
+    kata = storer.kata_exists?(id) ? dojo.katas[id] : nil
+    index = choose_language(languages_names, kata)
+    @start_points = ::DisplayNamesSplitter.new(languages_names, index)
+
+  def display_names_of(start_points)
+    start_points.map(&:display_name).sort
+  end
+
+  def choose_language(languages, kata)
+    chooser(languages, kata) { kata.display_name }
+  end
+
+  def chooser(choices, kata)
+    choice = [*0...choices.length].sample
+    unless kata.nil?
+      index = choices.index(yield)
+      choice = index unless index.nil?
+    end
+    choice
+  end
+
+See also app/lib/display_names_splitter.rb
+for DisplayNamesSplitter
+
+=end
