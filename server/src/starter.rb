5d19dccf7e511af6648280e3a0953eaa0898cf75
Jon Jagger <jon@jaggersoft.com>
2017-12-14 20:41:21 +0000

    prepared test-data and mounted into starter-server as readonly volume

--- server/src/starter.rb
+++ server/src/starter.rb
@@ -1,12 +1,35 @@
 
 class Starter
 
-  def initialize
-    puts "Starter"
-  end
-
   def method(a,b)
     a * b
+  end
+
+  def languages(major_name, minor_name)
+    #returns a major-list (languages) and a minot-list (testFrameworks)
+    #and initial indexes for both lists.
+  end
+
+  def exercises(exercise_name)
+    #returns a list of exercises
+    #and an initial index, given the 1 input name
+  end
+
+  def chose_language_exercise(major_name, minor_name, exercise_name)
+    #creates a new kata in the storer
+    #returns the new kata's hex-id
+  end
+
+  # - - - - - - - - - - - - - - - - -
+
+  def custom(major_name, minor_name)
+    #returns a major-list and a minor-list
+    #and initial indexes for both lists
+  end
+
+  def chose_custom()
+    #creates a new kata in the storer
+    #returns the new kata's hex-id
   end
 
 end
