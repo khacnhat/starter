ec30adc61d7b5ab0991e5aa949433904e93a3600
Jon Jagger <jon@jaggersoft.com>
2017-12-16 11:18:30 +0000

    added documentation to Indexer re matching current kata's display_name

--- server/src/indexer.rb
+++ server/src/indexer.rb
@@ -1,4 +1,48 @@
 
+# When you are setting up a new cyber-dojo practice session
+# cyber-dojo remembers the kata-id of the current practice session
+# and the initially selected choices are the same as the
+# current practice session (if they are still available).
+# This is to help re-inforce the idea of repetition.
+#
+# For example, if the languages display_names are
+#
+#   [ "C#,Moq", "C#,NUnit", "Java,JUnit", "Java,Mockito" ]
+#
+# and the current practice session's display_name is "C#, NUnit",
+# then the un-Indexed result of languages_choice() is:
+#
+#   {
+#     "major_names"  : [ "C#", "Java" ],
+#     "minor_names"  : [ "JUnit", "Mockito", "Moq", "NUnit" ],
+#     "minor_indexes": [ [2,3], [1,0] ]
+#   }
+#
+# The Indexer will alter this to:
+#
+#   {
+#     "major_names"  : [ "C#", "Java" ],
+#     "minor_names"  : [ "JUnit", "Mockito", "Moq", "NUnit" ],
+#     "minor_indexes": [ [3,2], [1,0] ],
+#     "major_index"  : 0
+#   }
+#
+# Specifically:
+#   major_name("C#,NUnit") == "C#"
+#   minor_name("C#,NUnit") == "NUnit"
+#
+#   o) It will add a 'major_index' entry such that
+#      major_names[major_index] == "C#"
+#      If there is no matching major_name
+#      (eg the current display_name is "Python, NUnit")
+#      it will set it to a random index into major_names
+#
+#   o) It will ensure that
+#      minor_names[minor_indexes[major_index][0]] == "NUnit"
+#      If there is no matching minor_name
+#      (eg the current display_name is "C#, py.test")
+#      it will leave minor_indexes unchanged.
+
 class Indexer
 
   def initialize(cache)
