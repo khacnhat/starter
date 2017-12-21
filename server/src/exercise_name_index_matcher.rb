93756a61502c3e144a5ff0a1420ce30e63a60b2b
Jon Jagger <jon@jaggersoft.com>
2017-12-21 20:08:11 +0000

    added new class for previous refactoring

---
+++ server/src/exercise_name_index_matcher.rb
@@ -0,0 +1,23 @@
+
+class ExerciseNameIndexMatcher
+
+  def initialize(cache)
+    @cache = cache
+  end
+
+  # - - - - - - - - - - - - - - - - -
+
+  def match(current_exercise_name)
+    names = cache[:names]
+    index = names.index(current_exercise_name)
+    if index.nil?
+      index = rand(0...names.size)
+    end
+    cache[:index] = index
+  end
+
+  private # = = = = = = = = = = = = = = = =
+
+  attr_reader :cache
+
+end
