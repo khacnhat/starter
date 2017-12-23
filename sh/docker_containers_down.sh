54e129adcf13a871e0117c37feaa3ee9ecb90f29
Jon Jagger <jon@jaggersoft.com>
2017-12-23 12:12:44 +0000

    added belt-and-braces options to docker-compose calls

--- sh/docker_containers_down.sh
+++ sh/docker_containers_down.sh
@@ -6,4 +6,5 @@ readonly ROOT_DIR="$( cd "$( dirname "${0}" )" && cd .. && pwd )"
 docker-compose \
   --file ${ROOT_DIR}/docker-compose.yml \
   down \
-  --remove-orphans
+  --remove-orphans \
+  --volumes
