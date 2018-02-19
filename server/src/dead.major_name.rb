b5236259b0ee345437531e51643e132a0201180a
Jon Jagger <jon@jaggersoft.com>
2018-02-19 13:27:38 +0000

    deleted dead code

--- server/src/dead.major_name.rb
+++
@@ -1,8 +0,0 @@
-
-module MajorName
-
-  def major_name(display_name)
-    display_name.split(',')[0].strip
-  end
-
-end
--- server/src/dead.minor_name.rb
+++
@@ -1,8 +0,0 @@
-
-module MinorName
-
-  def minor_name(display_name)
-    display_name.split(',')[1].strip
-  end
-
-end
